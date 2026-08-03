44bb289d78ec624ec3a02a3edc16975a3d9ea496
anubansiwal10 <ab6941@srmist.edu.in>
2026-08-03 11:56:33 +0530

    Time: 5 ms (80.94%) | Memory: 46.1 MB (16.76%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int digit=0;
+        int reverse=0;
+        int number=x;
+        if (x<0){
+            return false;
+        }
+        else{
+        while(number!=0){
+            digit=number%10;
+            reverse=reverse*10+digit;
+            number=number/10;
+        }     
+        }
+        if(reverse==x){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
